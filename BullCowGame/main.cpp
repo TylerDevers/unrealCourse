90576e93bd0d1419707c47200b70eb1839cd6e62
tyler <devers.tyler@gmail.com>
2019-04-30 11:59:11 -0700

    introduced game in main.cpp, BUlls and Cows

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
+#include <string>
+
+using namespace std;
 
 int main()
 {
+    // Introduce the game
+    constexpr int WORD_LENGTH = 5; //constant expression used when values will not change, similar to a constant but stronger.
+    cout << "Welcome to Bulls and Cows, a fun word game.";
+    cout << std::endl; //same as the /n after the string, same as cout << "text here" << endl
+    //alternate syntax std::cout when not using namespace
+
+    cout << "Can you guess the " << WORD_LENGTH;
+    cout << " letter isogram I am thinking of?\n";
+
+    // Get a guess from player
+    string Guess = "";
+    cin >> Guess;
+
+    // repeat the guess back to them
     /* code */
     return 0;
 }
+
+
+
+
